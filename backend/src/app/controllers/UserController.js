03c9fea7ace9536fdc75575e0843cce3a0844758
Wallace <wbescrovaine@gmail.com>
2020-10-18 01:19:58 -0300

    Controller de Usuário

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -5,13 +5,17 @@ class UserController {
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
-      // return res.status(400).json({ error: 'user@AlredyExists' });
-      return res.status(400).json('deu zica');
+      return res.status(400).json({ error: 'User already exists' });
     }
 
-    const user = await User.create(req.body);
+    const { id, name, email, provider } = await User.create(req.body);
 
-    return res.json(user);
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 export default new UserController();
