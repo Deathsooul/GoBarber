be9989de1a9ae86aebd14126312f4f55d2a5a8fa
Wallace <wbescrovaine@gmail.com>
2020-10-20 02:03:23 -0300

    Listagem + relacionamento

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,10 +2,13 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
+// Controllers
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
+// Middleware
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -17,6 +20,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware); // tudo que vier daqui pra baixo vai passar pela authenticação
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
