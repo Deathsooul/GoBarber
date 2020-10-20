c28ede49c29081baf4c69a79958f45a594da7577
Wallace <wbescrovaine@gmail.com>
2020-10-20 01:45:50 -0300

    Integração do avatar de usuário

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,10 +1,11 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -15,7 +16,11 @@ class Database {
     // Ja tem a conexão com o banco de dados
     this.connection = new Sequelize(databaseConfig);
 
-    models.map((model) => model.init(this.connection));
+    models
+      .map((model) => model.init(this.connection))
+      .map(
+        (model) => model.associate && model.associate(this.connection.models)
+      );
   }
 }
 
