f4218cb77143d68df6314600f4f91561e00b9a1b
Wallace <wbescrovaine@gmail.com>
2020-10-29 19:40:20 -0300

    Reconfigurando banco e sequelize

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: '192.168.100.34',
   username: 'postgres',
   password: 'docker',
-  databse: 'gobarber',
+  database: 'gobarber',
   define: {
     timestamps: true,
     underscored: true,
