25486b733d44e558976be74604a3a0a2228f1c13
Wallace <wbescrovaine@gmail.com>
2020-10-31 19:40:03 -0300

    Model de agendamento

---
+++ backend/src/app/models/Appointment.js
@@ -0,0 +1,23 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Appointment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      { sequelize }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    // quando possui dois relacionamentos com uma mesma tabela é obrigatorio usar o "as"
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
+  }
+}
+
+export default Appointment;
