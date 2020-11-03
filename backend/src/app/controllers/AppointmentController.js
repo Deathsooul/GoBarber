98bb590bb237f731a31a43a74fe75751818cc0dd
Wallace <wbescrovaine@gmail.com>
2020-11-02 22:01:58 -0300

    Paginação

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -6,9 +6,14 @@ import File from '../models/File';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
+      limit: 20,
+      /* Quantidade de registros que vai pular e listar por vez */
+      offset: (page - 1) * 20,
       attributes: ['id', 'date'],
       include: [
         {
